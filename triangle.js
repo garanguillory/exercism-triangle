5705cec8ce5399eacc64bc3dd79081f9bbfd6abf
Garan Guillory <garanguillory@gmail.com>
2016-02-09 20:46:14 -0700

    All this.kind() tests are passing

--- triangle.js
+++ triangle.js
@@ -5,18 +5,38 @@
 	// The sum of the lengths of any two sides of a triangle
 	// must be greater than the third side.
 
-var Triangle = function(input){
+var Triangle = function(a,b,c){
 
-	// var array = [];
-	// array.push(agruments);
-	// return array;
+	this.sortArgs = function() {
+	    var args = [];
+	    for (var i = 0; i < arguments.length; i++)
+	        args[i] = arguments[i];
+	    return args.sort().reverse();
+	};
+
+	this.unique = function(collection){
+    return collection.filter(function(el, pos){
+        return collection.indexOf(el) === pos;
+    });
+	};
+
 
 	this.kind = function(){
-		var array = [];
-			this.agruments.map(function(element){
-				array.push(element);
-			});
-		return array;
+		var array = this.sortArgs(a,b,c);
+		var sides = this.unique(array);
+
+			if(sides.length == 1){
+				return 'equilateral';
+			} else if(sides.length == 2){
+				return 'isosceles';
+			} else if(sides.length == 3 && (array[1]+array[2] > array[0])){
+				return 'scalene';
+			}
+
+		// var c = array[0];
+		// var b = array[1];
+		// var a = array[2];
+		
 	};
 
 };
@@ -25,3 +45,4 @@ var Triangle = function(input){
 
 
 module.exports = Triangle;
+
